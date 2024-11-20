e13c75ae0fd7b5386b040c5035857a322c4377a0
Sandip Chaudhary <chaudharysandip124@gmail.com>
2024-11-20 11:09:58 +0100

    fix: alternate

--- alternate/Alternate.py
+++ alternate/Alternate.py
@@ -1,5 +1,13 @@
 import networkx as nx
 
+def solve(n, m, r, s, t, vertices: list[str], edges: list[str]) -> bool:
+    if n <= 0 or m <= 0 or s == t:
+        return False
+
+    G,s,t = create_network(n, m, r, s, t, vertices, edges)
+
+    return find_path_alternate(G, s, t)
+
 def alternate(Graph):
 
     Nodes = nx.get_node_attributes(Graph, 'color')
@@ -31,3 +39,30 @@ def find_path_alternate(Graph, Start, Destination):
     G = alternate(Graph)
     
     return find_path(G, Start, Destination)
+
+def create_network(n, m, r, s, t, vertices: list[str], edges: list[str]):
+
+    G = nx.Graph()
+        
+    Nodes_End = n + 2
+    for vertex_line in vertices:
+
+        Node = vertex_line.split()
+        
+        if len(Node) == 1: 
+            G.add_node(Node[0], color = "black")
+        
+        else:
+            G.add_node(Node[0], color = "red")
+    
+
+    if "->" in edges[0]:
+        G.to_directed()
+        
+    for edge in edges: 
+
+        Edge = edge.split()
+
+        G.add_edge(Edge[0], Edge[2])
+
+    return [G, s, t]
--- alternate/Main.py
+++
@@ -1,7 +0,0 @@
-import Alternate as A
-import Graph_Creation as GC
-import networkx as nx
-
-
-G, s, t = GC.create_network("G-ex.txt")
-print(A.find_path_alternate(G, s, t))
